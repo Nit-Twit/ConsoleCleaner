dbf44aa78b01ab4c9d76a4e3241b78c4b4d80890
Nit-Twit <nhsjrotccyber@gmail.com>
2024-10-22 13:18:45 -0400

    Console Cleaner v1.0.1

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # ConsoleCleaner
+
+ConsoleCleaner is an npm package designed to help developers clean up and manage console logs in their JavaScript applications.
+
+## Usage
+
+Simply run the cli
+
+```bash
+$ npx console-cleaner
+```
+
+### Config
+
+There are currently two config options: `verbose` and `ignore`. 
+To run the cli with config create a file called `.cleanerconfig.json`.
+<br>Here's an example of what your config might look like:
+
+```json
+{
+    "verbose": true,
+    "ignore": ["index.js", "*.ts"]
+}
+```
+This would exclude the file `index.js` and every file ending in `.ts`
+
+**By default the tool automatically excludes the node_modules folder and it's contents**
+
+## Contributing
+
+Contributions are welcome! Please open an issue or submit a pull request on GitHub.
+
+## License
+
+This project is licensed under the MIT License.
--- index.js
+++ index.js
@@ -17,20 +17,23 @@ const cleanerConfigFile = await glob(".cleanerconfig.json", {
 if (cleanerConfigFile.length === 0 || cleanerConfigFile === 0) {
   rl.question(
     chalk.yellowBright(
-      "No .cleanerconfig file was found, do you want to make one? (Y/n)"
+      "No .cleanerconfig.json file was found, do you want to make one? (Y/n)"
     ),
     async (answer) => {
       if (answer.toLowerCase() === "y" || answer === "") {
         console.log(
           chalk.greenBright(
-            "Creating a .cleanerconfig file with default settings...\n(Rerun the command)"
+            "Creating a .cleanerconfig.json file with default settings...\n(Rerun the command)"
           )
         );
         const defaultConfig = {
           verbose: false,
           ignore: [],
         };
-        await fs.writeFileSync(".cleanerconfig.json", JSON.stringify(defaultConfig, null, 2));
+        await fs.writeFileSync(
+          ".cleanerconfig.json",
+          JSON.stringify(defaultConfig, null, 2)
+        );
         process.exit(0);
       }
     }
@@ -38,8 +41,7 @@ if (cleanerConfigFile.length === 0 || cleanerConfigFile === 0) {
 } else {
   const rawConfig = await fs.readFileSync(".cleanerconfig.json", "utf-8");
   const config = JSON.parse(rawConfig);
-  
-  console.log(config);
+
   if (config.verbose) {
     console.log(chalk.greenBright("Verbose mode enabled"));
     if (config.ignore && config.ignore.length > 0) {
@@ -54,45 +56,43 @@ if (cleanerConfigFile.length === 0 || cleanerConfigFile === 0) {
         cwd: process.cwd(),
         ignore: ["node_modules", "node_modules/**", ...config.ignore],
       }).then((files) => files);
-    
+
       if (files.length === 0) {
         console.log(
           chalk.redBright(
-            "Error: Detected no files to modify. Check your .cleanerconfig file and try again."
+            "Error: Detected no files to modify. Check your .cleanerconfig.json file and try again."
           )
         );
         process.exit(0);
       }
-    
+
       console.log(
         chalk.cyanBright(
-          `Files to be modified:\n\t${chalk.whiteBright("\u2022")} ${chalk.magentaBright(
+          `Files to be modified:\n\t${chalk.whiteBright(
+            "\u2022"
+          )} ${chalk.magentaBright(
             files.join("\n\t" + chalk.whiteBright("\u2022") + " ")
           )}\n`
         )
       );
-    
+
       const promises = files.map(async (file) => {
         try {
           const rawFile = await fs.promises.readFile(file, "utf-8");
-    
+
           const regex = /console\.log\s*\(\s*[\s\S]*?\);\s*/g; // Improved regex
           const modifiedFile = rawFile.replace(regex, ""); // Remove console.log
-    
-          // Debugging outputs
-          console.log("Original File Content:\n", rawFile);
-          console.log("Modified File Content:\n", modifiedFile);
-    
+
           const matches = rawFile.match(regex);
-          const count = matches ? matches.length + " Statement(s) removed." : "Nothing"; 
-    
+          const count = matches
+            ? matches.length + " Statement(s) removed"
+            : "Nothing removed";
+
           await fs.promises.writeFile(file, modifiedFile);
-    
+
           if (config.verbose) {
             console.log(
-              chalk.greenBright(
-                `Successfully modified ${file} (${count})`
-              )
+              chalk.greenBright(`Successfully modified ${file} (${count})`)
             );
           }
         } catch (error) {
@@ -100,10 +100,110 @@ if (cleanerConfigFile.length === 0 || cleanerConfigFile === 0) {
           process.exit(0);
         }
       });
-    
+
       await Promise.all(promises);
       console.log(chalk.blueBright("\nSuccessfully modified all files"));
-      process.exit(0);    
+      process.exit(0);
+    } else {
+      console.log(
+        chalk.yellowBright(
+          `Modifying every file except those in the node_modules directory...`
+        )
+      );
+      const files = await glob("**/*.{ts,js}", {
+        cwd: process.cwd(),
+        ignore: ["node_modules", "node_modules/**"],
+      }).then((files) => files);
+
+      if (files.length === 0) {
+        console.log(
+          chalk.redBright(
+            "Error: Detected no files to modify. Check your .cleanerconfig.josn file and try again."
+          )
+        );
+        process.exit(0);
+      }
+
+      console.log(
+        chalk.cyanBright(
+          `Files to be modified:\n\t${chalk.whiteBright(
+            "\u2022"
+          )} ${chalk.magentaBright(
+            files.join("\n\t" + chalk.whiteBright("\u2022") + " ")
+          )}\n`
+        )
+      );
+
+      const promises = files.map(async (file) => {
+        try {
+          const rawFile = await fs.promises.readFile(file, "utf-8");
+
+          const regex = /console\.log\s*\(\s*[\s\S]*?\);\s*/g; // Improved regex
+          const modifiedFile = rawFile.replace(regex, ""); // Remove console.log
+
+          const matches = rawFile.match(regex);
+          const count = matches
+            ? matches.length + " Statement(s) removed"
+            : "Nothing removed";
+
+          await fs.promises.writeFile(file, modifiedFile);
+
+          if (config.verbose) {
+            console.log(
+              chalk.greenBright(`Successfully modified ${file} (${count})`)
+            );
+          }
+        } catch (error) {
+          console.log(chalk.redBright(`Error: ${error.message}`));
+          process.exit(0);
+        }
+      });
+
+      await Promise.all(promises);
+      console.log(chalk.blueBright("\nSuccessfully modified all files"));
+      process.exit(0);
+    }
+  } else {
+    if (config.ignore && config.ignore.length > 0) {
+      const files = await glob("**/*.{ts,js}", {
+        cwd: process.cwd(),
+        ignore: ["node_modules", "node_modules/**", ...config.ignore],
+      }).then((files) => files);
+      if (files.length === 0) {
+        process.exit(0);
+      }
+      const promises = files.map(async (file) => {
+        try {
+          const rawFile = await fs.promises.readFile(file, "utf-8");
+          const modifiedFile = rawFile.replace(/console\.log\(.*?\);?/g, "");
+          await fs.promises.writeFile(file, modifiedFile);
+        } catch (error) {
+          process.exit(0);
+        }
+      });
+      console.log(chalk.blueBright("\nSuccessfully modified all files!"));
+      await Promise.all(promises);
+      process.exit(0);
+    } else {
+      const files = await glob("**/*.{ts,js}", {
+        cwd: process.cwd(),
+        ignore: ["node_modules", "node_modules/**"],
+      }).then((files) => files);
+      if (files.length === 0) {
+        process.exit(0);
+      }
+      const promises = files.map(async (file) => {
+        try {
+          const rawFile = await fs.promises.readFile(file, "utf-8");
+          const modifiedFile = rawFile.replace(/console\.log\(.*?\);?/g, "");
+          await fs.promises.writeFile(file, modifiedFile);
+        } catch (error) {
+          process.exit(0);
+        }
+      });
+      console.log(chalk.blueBright("\nSuccessfully modified all files!"));
+      await Promise.all(promises);
+      process.exit(0);
     }
   }
 }
